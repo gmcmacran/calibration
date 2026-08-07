16bbc0de60721e54b9b49e3fa620e7f4b55dec68
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2026-08-07 16:33:22 -0500

    add results

--- R Code/one sample/empirical variance sim.R
+++ R Code/one sample/empirical variance sim.R
@@ -1,50 +1,54 @@
 library(LRTesteR)
 library(tidyverse)
 library(stringr)
-library(furrr)
 
 ################
 # Simulation settings
 ################
 compiler::enableJIT(3)
-plan(multisession, workers = 10)
 B <- 5000
 N <- 15 # 15
 
 ################
 # Type I
 ################
-variances <- seq(.5, 3, .50)
+variances <- 1
 
-run_sim <- function(variances) {
-  sim_results <- tibble()
-  for (variance in variances) {
-    for (alt in c("two.sided", "less", "greater")) {
-      stats <- vector(mode = "numeric", length = B)
-      pvalues <- vector(mode = "numeric", length = B)
-      alts <- vector(mode = "character", length = B)
-      CI_LBs <- vector(mode = "numeric", length = B)
-      CI_UBs <- vector(mode = "numeric", length = B)
-      testName <- "empirical_variance_test"
-      for (i in 1:B) {
-        set.seed(i)
-        x <- rnorm(n = N, mean = 0, sd = variance^.5)
-        test <- empirical_variance_test(x, variance, alt)
-        stats[i] <- test$statistic
-        pvalues[i] <- test$p.value
-        alts[i] <- test$alternative
-        CI_LBs[i] <- test$conf.int[1]
-        CI_UBs[i] <- test$conf.int[2]
-      }
-      temp <- tibble(test = testName, mu = 0, variance = variance, stat = stats, pvalue = pvalues, alt = alts, CI_LB = CI_LBs, CI_UB = CI_UBs)
-      sim_results <- sim_results |> bind_rows(temp)
-      rm(stats, pvalues, alts, testName, temp, i, test, x, CI_LBs, CI_UBs)
-    }
-  }
-  return(sim_results)
+# The sample must have wide enough range.
+# Otherwise, the optimization behind the test cannot be solved.
+# If the optimization cannot be solved, take another sample.
+not_testable_x <- function(x, null_variance) {
+  result <- (max(x) - min(x)) / 4 < variance
+  return(result)
 }
 
-sim_results <- future_map_dfr(variances, run_sim, .options = furrr_options(seed = TRUE))
+sim_results <- tibble()
+for (variance in variances) {
+  for (alt in c("two.sided", "less", "greater")) {
+    stats <- vector(mode = "numeric", length = B)
+    pvalues <- vector(mode = "numeric", length = B)
+    alts <- vector(mode = "character", length = B)
+    CI_LBs <- vector(mode = "numeric", length = B)
+    CI_UBs <- vector(mode = "numeric", length = B)
+    testName <- "empirical_variance_test"
+    for (i in 1:B) {
+      set.seed(i)
+      x <- rnorm(n = N, mean = 0, sd = variance^.5)
+      while (not_testable_x(x, variance)) {
+        x <- rnorm(n = N, mean = 0, sd = variance^.5)
+      }
+      test <- empirical_variance_test(x, variance, alt)
+      stats[i] <- test$statistic
+      pvalues[i] <- test$p.value
+      alts[i] <- test$alternative
+      CI_LBs[i] <- test$conf.int[1]
+      CI_UBs[i] <- test$conf.int[2]
+    }
+    temp <- tibble(test = testName, mu = 0, variance = variance, stat = stats, pvalue = pvalues, alt = alts, CI_LB = CI_LBs, CI_UB = CI_UBs)
+    sim_results <- sim_results |> bind_rows(temp)
+    rm(stats, pvalues, alts, testName, temp, i, test, x, CI_LBs, CI_UBs)
+  }
+}
 
 # Check structure
 sim_results |>
@@ -75,5 +79,4 @@ sim_results |>
 sim_results |>
   saveRDS("results/empirical_variance_type_one.rds")
 
-plan(sequential)
 rm(list = ls())
--- R Code/two sample/empirical variance two sample.R
+++ R Code/two sample/empirical variance two sample.R
@@ -1,13 +1,11 @@
 library(LRTesteR)
 library(tidyverse)
 library(stringr)
-library(furrr)
 
 ################
 # Simulation settings
 ################
 compiler::enableJIT(3)
-plan(multisession, workers = 5)
 B <- 5000
 N <- 30 # 30
 
@@ -15,32 +13,27 @@ N <- 30 # 30
 # Type I
 ################
 mu <- 0
-variances <- seq(1, 4, 1)
+variances <- 1
 
-run_sim <- function(variances) {
-  sim_results <- tibble()
-  for (variance in variances) {
-    stats <- vector(mode = "numeric", length = B)
-    pvalues <- vector(mode = "numeric", length = B)
-    alts <- vector(mode = "character", length = B)
-    testName <- "empirical_variance_one_way_test"
-    for (i in 1:B) {
-      set.seed(i)
-      x <- rnorm(n = N, mean = mu, sd = variance^.5)
-      fctr <- factor(c(rep("1", N / 2), rep("2", N / 2)), levels = c("1", "2"))
-      test <- empirical_variance_one_way_test(x, fctr)
-      stats[i] <- test$statistic
-      pvalues[i] <- test$p.value
-      alts[i] <- test$alternative
-    }
-    temp <- tibble(test = testName, mu = mu, variance = variance, stat = stats, pvalue = pvalues, alt = alts)
-    sim_results <- sim_results |> bind_rows(temp)
-    rm(stats, pvalues, alts, testName, temp, i, fctr, x, test)
+sim_results <- tibble()
+for (variance in variances) {
+  stats <- vector(mode = "numeric", length = B)
+  pvalues <- vector(mode = "numeric", length = B)
+  alts <- vector(mode = "character", length = B)
+  testName <- "empirical_variance_one_way_test"
+  for (i in 1:B) {
+    set.seed(i)
+    x <- rnorm(n = N, mean = mu, sd = variance^.5)
+    fctr <- factor(c(rep("1", N / 2), rep("2", N / 2)), levels = c("1", "2"))
+    test <- empirical_variance_one_way_test(x, fctr)
+    stats[i] <- test$statistic
+    pvalues[i] <- test$p.value
+    alts[i] <- test$alternative
   }
-  return(sim_results)
+  temp <- tibble(test = testName, mu = mu, variance = variance, stat = stats, pvalue = pvalues, alt = alts)
+  sim_results <- sim_results |> bind_rows(temp)
+  rm(stats, pvalues, alts, testName, temp, i, fctr, x, test)
 }
-
-sim_results <- future_map_dfr(variances, run_sim, .options = furrr_options(seed = TRUE))
 
 # Check structure
 sim_results |>
@@ -71,5 +64,4 @@ sim_results |>
 sim_results |>
   saveRDS("results/empirical_variance_type_one_one_way.rds")
 
-plan(sequential)
 rm(list = ls())
